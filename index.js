0f5a1dd701dd515716d0b274dffc55d68a395b05
Tomas Grubliauskas <v3nom@MacPro.local>
2018-10-14 16:45:50 +0200

    removed blog recipe

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 #!/usr/bin/env node
-var uSite = require('./dist/usite');
+var path = require('path');
 
 var command = process.argv[2];
+var param = process.argv[3];
 
 if (command == 'generate') {
-    require('./recipies/blog');
+    if (!param) {
+        console.error('Filename is required when calling generate. Example: "usite generate blog"');
+        return;
+    }
+
+    var generator = require(path.resolve(process.cwd(), param));
+    if (!generator.generate) {
+        console.error("Provided generator is missing generate function");
+        return;
+    }
+
+    var uSite = require("./dist/usite");
+    generator.generate(uSite.default);
 }
 
 if (command == 'init') {
-    require('./recipies/init');
+    var fse = require('fs-extra');
+    var dirPath = __dirname;
+    var demoPath = path.resolve(dirPath, './node_modules/usiteDemo/');
+    var workingDirectory = process.cwd();
+
+    fse.copySync(demoPath, workingDirectory);
 }
